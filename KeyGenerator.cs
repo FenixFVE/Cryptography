26aa0d430440c71a23f2aacc67eebdf8c943425e
Vlad <Vlad@DESKTOP-7VHS6B5>
2022-09-04 20:03:57 +0700

    Add project files.

---
+++ KeyGenerator.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Text;
+
+namespace Cryptography
+{
+    public class KeyGenerator
+    {
+        
+        public static string RandomRussianKey()
+        {
+            List<char> let = Enumerable.Range(0, 32).Select((x, i) => (char)('а' + i)).ToList();
+            let.Insert(6, 'ё');
+            Random random = new Random();
+            return new string(let.OrderBy(x => random.Next()).ToArray());
+        }
+
+        public static void CreateRussianKey(string fileName)
+        {
+            using (var sw = new StreamWriter(File.Open(fileName, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                string russianKey = RandomRussianKey();
+                sw.WriteLine(russianKey);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using Cryptography;
+using System.Text;
+
+//Console.OutputEncoding = Encoding.UTF8;
+
+KeyGenerator.CreateRussianKey("test.txt");
