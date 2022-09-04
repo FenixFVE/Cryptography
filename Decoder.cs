e7ce784fd10041c75aacce4ed6d6f204ab8964f8
Fedorov Vladislav <NextFenixFVE@gmail.com>
2022-09-04 20:44:42 +0700

    de(en)coder

---
+++ Decoder.cs
@@ -0,0 +1,10 @@
+﻿
+using System;
+
+namespace Cryptography
+{
+    public class Decoder
+    {
+         
+    }
+}
---
+++ Encoder.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Cryptography
+{
+    public class Encoder
+    {
+        public static void TextFormater(string inputFile, string outputFile)
+        {
+
+        }
+    }
+}
